9bd1326d424b8a18bc47a4ab4fc5567709d3621e
shadowhacrr <ayeshatt667788@gmail.com>
2026-04-27 01:44:43 +0500

    Update config path in messages.js

--- utils/messages.js
+++ utils/messages.js
@@ -2,7 +2,7 @@
 // 🎨 UI MESSAGES & TEMPLATES
 // ═══════════════════════════════════════════════════════════
 
-const { CONFIG } = require("./config");
+const { CONFIG } = require("../config");
 
 const ICONS = {
   bot: "🤖",
